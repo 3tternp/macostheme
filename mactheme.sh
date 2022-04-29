62f8b29960b735b92c91bbd8a7e6faac9283dbf7
astra <punkx.0gravity@gmail.com>
2022-04-29 12:43:34 +0545

    Add files via upload

---
+++ mactheme.sh
@@ -0,0 +1,11 @@
+#! /bin/bash 
+sudo mkdir ~/.themes
+sudo mkdir ~/.icons 
+cd ~/.themes
+sudo git clone https://github.com/B00merang-Project/macOS
+sudo https://github.com/B00merang-Project/macOS-Catalina-Dark
+cd ..
+cd .icons
+
+sudo git clone https://github.com/keeferrourke/la-capitaine-icon-theme
+
